8e4e9a10f2d5ea4509f8d843f3d6216555b65df6
deb3bansen <46204885+deb3bansen@users.noreply.github.com>
2019-09-10 19:35:35 -0300

    Add files via upload

---
+++ Calculo de x/src/FuncaoDeX.java
@@ -0,0 +1,14 @@
+
+public class FuncaoDeX {	
+	public void FuncaoDeX(int x){
+    	if(x<5&&x>=0)
+    		System.out.println(x);
+    	if(x>=5&&x<10)
+    		System.out.println(2*x+1);
+    	if(x>=10)
+    		System.out.println(x-3);
+    	
+		
+    }
+
+}
---
+++ Calculo de x/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner (System.in);
+		System.out.println("digite um numero:");
+		//FuncaoDeX  x=new FuncaoDeX();
+		int x=in.nextInt();
+		FuncaoDeX n=new FuncaoDeX();
+		n.FuncaoDeX(x);
+
+	}
+
+}

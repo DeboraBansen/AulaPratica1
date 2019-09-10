93f071f442854f36fece3ef7003ad359f85e3630
deb3bansen <46204885+deb3bansen@users.noreply.github.com>
2019-09-10 12:09:49 -0300

    Add files via upload

---
+++ Tabuada/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import calculo.Tabuada;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner (System.in);
+		Tabuada n;
+		System.out.println("  MULTIPLOS DE UM NUMERO ");
+		System.out.println("Digite um numero:");
+		n=new Tabuada(in.nextInt());
+
+	}
+
+	
+
+}
---
+++ Tabuada/src/calculo/Tabuada.java
@@ -0,0 +1,9 @@
+package calculo;
+
+public class Tabuada {
+      public  Tabuada(int n){
+    	  for(int i=0;i<=10;i++){
+    		  System.out.println(""+n+"x"+i+"="+n*i);
+    	  }
+      }
+}
